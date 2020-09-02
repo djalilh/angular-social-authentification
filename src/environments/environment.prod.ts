fb152c0c791047da2f4b6b3b2e911969015bb9f4
HATTI ABDELDJALIL <djalilhatti@gmail.com>
2020-09-02 17:15:38 +0100

    update env props

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  googleOauthClientID: "655833365118-ge1b87pigm6l44m2d3rorfnh082a6rce.apps.googleusercontent.com",
+  facebookOauthClientID: "",
 };
